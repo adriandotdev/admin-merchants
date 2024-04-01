bedf117ecb1a5067afe3a958971df9ac943054e0
Adrian <marcelonads@gmail.com>
2024-04-01 08:16:07 +0800

    refactor: SPECIFIED topup_logs.id instead of id to resolve ambiguity
    The method that I edit in MerchantRepository.js is GetTopupByID

--- repository/MerchantRepository.js
+++ repository/MerchantRepository.js
@@ -1,132 +1,132 @@
 const mysql = require("../database/mysql");
 
 module.exports = class MerchantRepository {
-  GetCPOs(data) {
-    const QUERY = `SELECT users.username, 
+	GetCPOs(data) {
+		const QUERY = `SELECT users.username, 
 	cpo_owners.* 
 	FROM cpo_owners 
 	INNER JOIN users 
 	ON cpo_owners.user_id = users.id
 	LIMIT ? OFFSET ?`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(
-        QUERY,
-        [parseInt(data.limit), parseInt(data.offset)],
-        (err, result) => {
-          if (err) {
-            reject(err);
-          }
+		return new Promise((resolve, reject) => {
+			mysql.query(
+				QUERY,
+				[parseInt(data.limit), parseInt(data.offset)],
+				(err, result) => {
+					if (err) {
+						reject(err);
+					}
 
-          resolve(result);
-        }
-      );
-    });
-  }
+					resolve(result);
+				}
+			);
+		});
+	}
 
-  RegisterCPO({
-    party_id,
-    cpo_owner_name,
-    contact_name,
-    contact_number,
-    contact_email,
-    username,
-    password,
-  }) {
-    const QUERY = `CALL WEB_ADMIN_REGISTER_CPO(?,?,?,?,?,?,?)`;
+	RegisterCPO({
+		party_id,
+		cpo_owner_name,
+		contact_name,
+		contact_number,
+		contact_email,
+		username,
+		password,
+	}) {
+		const QUERY = `CALL WEB_ADMIN_REGISTER_CPO(?,?,?,?,?,?,?)`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(
-        QUERY,
-        [
-          party_id,
-          cpo_owner_name,
-          contact_name,
-          contact_number,
-          contact_email,
-          username,
-          password,
-        ],
-        (err, result) => {
-          if (err) {
-            reject(err);
-          }
+		return new Promise((resolve, reject) => {
+			mysql.query(
+				QUERY,
+				[
+					party_id,
+					cpo_owner_name,
+					contact_name,
+					contact_number,
+					contact_email,
+					username,
+					password,
+				],
+				(err, result) => {
+					if (err) {
+						reject(err);
+					}
 
-          resolve(result);
-        }
-      );
-    });
-  }
+					resolve(result);
+				}
+			);
+		});
+	}
 
-  /**
-   * @param {String} cpoOwnerName
-   * @returns {Promise<Object>}
-   */
-  SearchCPOByName(cpoOwnerName) {
-    const QUERY = `
+	/**
+	 * @param {String} cpoOwnerName
+	 * @returns {Promise<Object>}
+	 */
+	SearchCPOByName(cpoOwnerName) {
+		const QUERY = `
 		SELECT users.username, cpo_owners.* 
 		FROM cpo_owners
 		INNER JOIN users
 		ON cpo_owners.user_id = users.id
 		WHERE LOWER(cpo_owner_name) LIKE ?`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(QUERY, [`%${cpoOwnerName}%`], (err, result) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(result);
-      });
-    });
-  }
+		return new Promise((resolve, reject) => {
+			mysql.query(QUERY, [`%${cpoOwnerName}%`], (err, result) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(result);
+			});
+		});
+	}
 
-  UpdateCPOByID({ id, query }) {
-    const QUERY = `UPDATE cpo_owners
+	UpdateCPOByID({ id, query }) {
+		const QUERY = `UPDATE cpo_owners
 		INNER JOIN users
 		ON cpo_owners.user_id = users.id
 		${query} WHERE cpo_owners.id = ?`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(QUERY, [id], (err, result) => {
-        if (err) {
-          reject(err);
-        }
+		return new Promise((resolve, reject) => {
+			mysql.query(QUERY, [id], (err, result) => {
+				if (err) {
+					reject(err);
+				}
 
-        resolve(result);
-      });
-    });
-  }
+				resolve(result);
+			});
+		});
+	}
 
-  AddRFID(cpoOwnerID, rfidCardTag) {
-    const QUERY = `CALL WEB_ADMIN_ADD_RFID(?,?)`;
+	AddRFID(cpoOwnerID, rfidCardTag) {
+		const QUERY = `CALL WEB_ADMIN_ADD_RFID(?,?)`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(QUERY, [cpoOwnerID, rfidCardTag], (err, result) => {
-        if (err) {
-          reject(err);
-        }
+		return new Promise((resolve, reject) => {
+			mysql.query(QUERY, [cpoOwnerID, rfidCardTag], (err, result) => {
+				if (err) {
+					reject(err);
+				}
 
-        resolve(result);
-      });
-    });
-  }
+				resolve(result);
+			});
+		});
+	}
 
-  Topup(cpoOwnerID, amount) {
-    const QUERY = `CALL WEB_ADMIN_TOPUP(?,?)`;
+	Topup(cpoOwnerID, amount) {
+		const QUERY = `CALL WEB_ADMIN_TOPUP(?,?)`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(QUERY, [cpoOwnerID, amount], (err, result) => {
-        if (err) {
-          reject(err);
-        }
+		return new Promise((resolve, reject) => {
+			mysql.query(QUERY, [cpoOwnerID, amount], (err, result) => {
+				if (err) {
+					reject(err);
+				}
 
-        resolve(result);
-      });
-    });
-  }
+				resolve(result);
+			});
+		});
+	}
 
-  GetTopupByID(cpoOwnerID) {
-    const QUERY = `SELECT *, DATE_ADD(topup_logs.date_created, INTERVAL 60 MINUTE) AS voidable_until
+	GetTopupByID(cpoOwnerID) {
+		const QUERY = `SELECT *, topup_logs.id, DATE_ADD(topup_logs.date_created, INTERVAL 60 MINUTE) AS voidable_until
 		FROM topup_logs
 		INNER JOIN cpo_owners
 		ON cpo_owners.user_id = topup_logs.user_id
@@ -135,28 +135,28 @@ module.exports = class MerchantRepository {
 		AND type = 'TOPUP'
 		AND void_id IS NULL`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(QUERY, [cpoOwnerID], (err, result) => {
-        if (err) {
-          reject(err);
-        }
+		return new Promise((resolve, reject) => {
+			mysql.query(QUERY, [cpoOwnerID], (err, result) => {
+				if (err) {
+					reject(err);
+				}
 
-        resolve(result);
-      });
-    });
-  }
+				resolve(result);
+			});
+		});
+	}
 
-  VoidTopup(referenceID) {
-    const QUERY = `CALL WEB_ADMIN_VOID_TOPUP(?)`;
+	VoidTopup(referenceID) {
+		const QUERY = `CALL WEB_ADMIN_VOID_TOPUP(?)`;
 
-    return new Promise((resolve, reject) => {
-      mysql.query(QUERY, referenceID, (err, result) => {
-        if (err) {
-          reject(err);
-        }
+		return new Promise((resolve, reject) => {
+			mysql.query(QUERY, referenceID, (err, result) => {
+				if (err) {
+					reject(err);
+				}
 
-        resolve(result);
-      });
-    });
-  }
+				resolve(result);
+			});
+		});
+	}
 };
